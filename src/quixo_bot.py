e8acfe586e02c0961c4d5a419271998fee354b0e
Cristian Aragón <crisarag8.sal@gmail.com>
2024-04-20 18:30:38 -0600

    Creación de estructura base de bot

---
+++ src/quixo_bot.py
@@ -0,0 +1,24 @@
+class QuixoBot:
+    def __init__(self, symbol):
+        pass
+    
+    def play_turn(self, board):
+        pass
+    
+    def __move_right(self, board, row, col, end_col=4):
+        pass
+    
+    def __move_left(self, board, row, col, end_col=0):
+        pass
+    
+    def __move_up(self, board, row, col, end_row=0):
+        pass
+    
+    def __move_down(self, board, row, col, end_row=4):
+        pass
+    
+    def __print_board(self):
+        pass
+    
+    def reset(self):
+        pass
