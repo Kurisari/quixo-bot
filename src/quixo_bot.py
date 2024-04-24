e4c9613211a01248bee9f4f97cf562c9fe85ef1b
Cristian Aragón <crisarag8.sal@gmail.com>
2024-04-23 20:31:18 -0600

    Primera implementación de movimientos con pruebas

--- src/quixo_bot.py
+++ src/quixo_bot.py
@@ -13,20 +13,50 @@ class QuixoBot:
         self.symbol = symbol
         self.board = [[0] * 5 for _ in range(5)]
     
-    def play_turn(self, board):
-        pass
+    def play_turn(self, board=None):
+        if board is None:
+            board = self.board
+        self.move_right(board, 1, 0)
+        self.move_right(board, 1, 0)
+        self.move_left(board, 2, 4)
+        self.move_left(board, 2, 4)
+        self.move_left(board, 3, 4)
+        self.move_right(board, 0, 0)
+        self.move_up(board, 2, 0)
+        self.move_right(board, 0, 0)
+        self.move_down(board, 0, 4)
     
-    def __move_right(self, board, row, col, end_col=4):
-        pass
+    def move_right(self, board, row, col, end_col=4):
+        piece = board[row][col]
+        if piece == 0 or piece == self.symbol:
+            if piece == 0:
+                board[row] = board[row][col + 1:] + [self.symbol]
+            else:
+                board[row] = board[row][col + 1:] + [piece]
+        self.print_board()
     
-    def __move_left(self, board, row, col, end_col=0):
-        pass
+    def move_left(self, board, row, col, end_col=0):
+        piece = board[row][col]
+        if piece == 0 or piece == self.symbol:
+            if piece == 0:
+                board[row] = [self.symbol] + board[row][:col]
+            else:
+                board[row] = [piece] + board[row][:col]
+        self.print_board()
     
-    def __move_up(self, board, row, col, end_row=0):
-        pass
+    def move_up(self, board, row, col, end_row=0):
+        piece = board[row][col]
+        if piece == 0 or piece == self.symbol:
+            for i in range(row, end_row - 1, -1):
+                board[i][col] = board[i - 1][col] if i != 0 else self.symbol
+        self.print_board()
     
-    def __move_down(self, board, row, col, end_row=4):
-        pass
+    def move_down(self, board, row, col, end_row=4):
+        piece = board[row][col]
+        if piece == 0 or piece == self.symbol:
+            for i in range(row, end_row + 1):
+                board[i][col] = board[i + 1][col] if i != 4 else self.symbol
+        self.print_board()
     
     def print_board(self):
         headers = [""] + [str(i) for i in range(1, 6)]
@@ -37,4 +67,4 @@ class QuixoBot:
         pass
 
 prueba = QuixoBot(1)
-prueba.print_board()
+prueba.play_turn()
