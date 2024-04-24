7df57e29602fc7aedb41835ebfc59ceb90333d5a
Cristian Aragón <crisarag8.sal@gmail.com>
2024-04-23 19:14:09 -0600

    Implementación de print_board

--- src/quixo_bot.py
+++ src/quixo_bot.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from tabulate import tabulate
 
 script_dir = os.getcwd()
 func_dir = os.path.join(script_dir)
@@ -9,7 +10,8 @@ from lib import quixo_lib as ql
 
 class QuixoBot:
     def __init__(self, symbol):
-        pass
+        self.symbol = symbol
+        self.board = [[0] * 5 for _ in range(5)]
     
     def play_turn(self, board):
         pass
@@ -26,8 +28,13 @@ class QuixoBot:
     def __move_down(self, board, row, col, end_row=4):
         pass
     
-    def __print_board(self):
-        pass
+    def print_board(self):
+        headers = [""] + [str(i) for i in range(1, 6)]
+        rows = [[str(i + 1)] + ['O' if cell == 1 else 'X' if cell == 2 else ' ' for cell in row] for i, row in enumerate(self.board)]
+        print(tabulate(rows, headers=headers, tablefmt="grid"))
     
     def reset(self):
         pass
+
+prueba = QuixoBot(1)
+prueba.print_board()
